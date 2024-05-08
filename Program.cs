c99a167ac9433aaeba3c706d6c032327e342f241
WilliamWiernes <williambrandonwiernes@gmail.com>
2024-05-07 22:33:29 -0300

    CalculadoraV2

--- Program.cs
+++ Program.cs
@@ -1,45 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
 
-/*int a;
-int b;
+// /*int a;
+// int b;
 
-a = 10; 
-b = a;
+// a = 10; 
+// b = a;
 
-Console.WriteLine("valor de a: "+a);
-Console.WriteLine("valor de b: "+b);
-*/
+// Console.WriteLine("valor de a: "+a);
+// Console.WriteLine("valor de b: "+b);
+// */
 
-string a;
-int num, aux;
-int numInv;
-int i;
+// string a;
+// int num, aux;
+// int numInv;
+// int i;
 
-numInv = 0;
-i = 0;
+// numInv = 0;
+// i = 0;
 
-Console.WriteLine("Ingrese un numero: ");
-a = Console.ReadLine();
-num = int.Parse(a);
+// Console.WriteLine("Ingrese un numero: ");
+// a = Console.ReadLine();
+// num = int.Parse(a);
 
 
-while (num != 0)
-{
-    aux = num % 10;
-    num = (int)num / 10;
+// while (num != 0)
+// {
+//     aux = num % 10;
+//     num = (int)num / 10;
 
-    if (i == 0)
-    {
-        numInv = aux;
+//     if (i == 0)
+//     {
+//         numInv = aux;
 
-    }
-    else{
-        numInv = numInv * 10 + aux;
+//     }
+//     else{
+//         numInv = numInv * 10 + aux;
         
-    }
-    i++;
-}
+//     }
+//     i++;
+// }
 
-Console.WriteLine("Numero Invertido: " +numInv);
+// Console.WriteLine("Numero Invertido: " +numInv);
 
